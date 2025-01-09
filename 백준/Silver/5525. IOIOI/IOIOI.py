f12db2e3ab6173a90891230990194ab3521796f9
snowball03 <qrpl9783@gmail.com>
2025-01-09 10:48:55 +0900

    [Silver I] Title: IOIOI, Time: 292 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,23 +1,24 @@
 n = int(input())
 l = int(input())
 s = input()
-cnt = 0
-j = 0
+cnt = 0  # P_N 패턴 개수
+j = 0  # 탐색 인덱스
 
-while j <= l - (2 * n + 1):
-    match = True
-    for k in range(2 * n + 1):
-        if k % 2 == 0 and s[j + k] != 'I':
-            match = False
-            break
-        elif k % 2 == 1 and s[j + k] != 'O':
-            match = False
-            break
-    if match:
-        cnt += 1
-        j += 2  # 중복 방지
+while j < l - 1:
+    # IO 패턴이 나타날 때
+    if s[j] == "I" and s[j + 1] == "O":
+        length = 0  # IOI 길이를 추적
+        while j < l - 1 and s[j] == "I" and s[j + 1] == "O":
+            length += 1
+            j += 2  # IO는 2칸씩 이동
+            # 다음 "I"를 확인
+            if j < l and s[j] == "I" and length >= n:
+                cnt += 1  # P_N 패턴 완성
+
+        # 반복이 끝나면 "I" 이후로 이동
     else:
         j += 1
 
 print(cnt)
+
 
